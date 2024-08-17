74ed878acd5e0afb8aa3a0c15c323f836c781c9d
jas777 <jas.czeszejko@gmail.com>
2024-08-17 21:14:40 +0200

    4-bit CLABA adder test fixed

--- src/main.zig
+++ src/main.zig
@@ -43,12 +43,9 @@ test "adder" {
         .{ true, true },
     };
 
-    // _ =outputs;
-
     testutils.testTitle("Full adder test");
 
     for (0.., input_scenarios) |i, input_scenario| {
-        // _ = i;
         var simulator = try Simulator.init(text_netlist, std.testing.allocator);
         defer simulator.deinit();
         
@@ -154,105 +151,110 @@ test "adder" {
 //     }
 // }
 
-// test "4-bit carry lookahead binary adder" {
-//     const text_netlist: [*:0]const u8 = 
-//         \\4-bit carry lookahead binary adder
-//         \\IN    : in_a0 in_a1 in_a2 in_b0 in_b1 in_b2 in_b3 in_carry
-//         \\OUT   : xor_7 xor_6 xor_5 xor_4 or_3
-//         \\XOR   : in_a0 in_b0                       -> xor_0
-//         \\AND   : in_a0 in_b0                       -> and_0
-//         \\XOR   : in_a1 in_b1                       -> xor_1
-//         \\AND   : in_a1 in_b1                       -> and_1
-//         \\XOR   : in_a2 in_b2                       -> xor_2
-//         \\AND   : in_a2 in_b2                       -> and_2
-//         \\XOR   : in_a3 in_b3                       -> xor_3
-//         \\AND   : in_a3 in_b3                       -> and_3
-//         \\XOR   : xor_0 in_carry                    -> xor_4
-//         \\XOR   : or_0 xor_1                        -> xor_5
-//         \\XOR   : or_1 xor_2                        -> xor_6
-//         \\XOR   : or_2 xor_3                        -> xor_7
-//         \\OR    : and_0 and_4                       -> or_0
-//         \\OR    : and_1 and_5 and_6                 -> or_1
-//         \\OR    : and_2 and_7 and_8 and_9           -> or_2
-//         \\OR    : and_3 and_10 and_11 and_12 and_13 -> or_3
-//         \\AND   : in_carry xor_0                    -> and_4
-//         \\AND   : and_0 xor_1                       -> and_5
-//         \\AND   : xor_0 xor_1 in_carry              -> and_6
-//         \\AND   : xor_2 and_1                       -> and_7
-//         \\AND   : xor_2 xor_1 and_0                 -> and_8
-//         \\AND   : xor_2 xor_1 xor_0 in_carry        -> and_9
-//         \\AND   : xor_3 and_2                       -> and_10
-//         \\AND   : xor_3 xor_2 and_1                 -> and_11
-//         \\AND   : xor_3 xor_2 xor_1 and_0           -> and_12
-//         \\AND   : xor_3 xor_2 xor_1 xor_0 in_carry  -> and_13
-//     ;
+test "4-bit carry lookahead binary adder" {
+    const text_netlist: []const u8 = 
+        \\@TOP 4bit_claba {
+        \\@IN    : in_a0 in_a1 in_a2 in_a3 in_b0 in_b1 in_b2 in_b3 in_carry;
+        // \\OUT   : xor_7 xor_6 xor_5 xor_4 or_3
+        \\XOR   : in_a0 in_b0                       -> xor_0;
+        \\AND   : in_a0 in_b0                       -> and_0;
+        \\XOR   : in_a1 in_b1                       -> xor_1;
+        \\AND   : in_a1 in_b1                       -> and_1;
+        \\XOR   : in_a2 in_b2                       -> xor_2;
+        \\AND   : in_a2 in_b2                       -> and_2;
+        \\XOR   : in_a3 in_b3                       -> xor_3;
+        \\AND   : in_a3 in_b3                       -> and_3;
+        \\XOR   : xor_0 in_carry                    -> xor_4;
+        \\XOR   : or_0 xor_1                        -> xor_5;
+        \\XOR   : or_1 xor_2                        -> xor_6;
+        \\XOR   : or_2 xor_3                        -> xor_7;
+        \\OR    : and_0 and_4                       -> or_0;
+        \\OR    : and_1 and_5 and_6                 -> or_1;
+        \\OR    : and_2 and_7 and_8 and_9           -> or_2;
+        \\OR    : and_3 and_10 and_11 and_12 and_13 -> or_3;
+        \\AND   : in_carry xor_0                    -> and_4;
+        \\AND   : and_0 xor_1                       -> and_5;
+        \\AND   : xor_0 xor_1 in_carry              -> and_6;
+        \\AND   : xor_2 and_1                       -> and_7;
+        \\AND   : xor_2 xor_1 and_0                 -> and_8;
+        \\AND   : xor_2 xor_1 xor_0 in_carry        -> and_9;
+        \\AND   : xor_3 and_2                       -> and_10;
+        \\AND   : xor_3 xor_2 and_1                 -> and_11;
+        \\AND   : xor_3 xor_2 xor_1 and_0           -> and_12;
+        \\AND   : xor_3 xor_2 xor_1 xor_0 in_carry  -> and_13;
+        \\}
+    ;
 
-//     var input_scenarios: [512][9] bool = undefined;
-//     var outputs: [512][5] bool = undefined;
+    var input_scenarios: [512][9] bool = undefined;
+    var outputs: [512][5] bool = undefined;
 
-//     testutils.testTitle("4-bit CLABA test");
+    testutils.testTitle("4-bit CLABA test");
 
-//     for (0..512) |i| {
-//         const a = i >> 5;
-//         const b = (i >> 1) & 0xF;
-//         const carry = i & 0x1;
+    for (0..512) |i| {
+        const a = i >> 5;
+        const b = (i >> 1) & 0xF;
+        const carry = i & 0x1;
 
-//         input_scenarios[i] = .{ a & 0x8 == 8, a & 0x4 == 4, a & 0x2 == 2, a & 0x1 == 1, b & 0x8 == 8, b & 0x4 == 4, b & 0x2 == 2, b & 0x1 == 1, carry == 1 };
+        input_scenarios[i] = .{ a & 0x8 == 8, a & 0x4 == 4, a & 0x2 == 2, a & 0x1 == 1, b & 0x8 == 8, b & 0x4 == 4, b & 0x2 == 2, b & 0x1 == 1, carry == 1 };
 
-//         const sum = a + b + carry;
-//         outputs[i] = .{ sum & 0x8 == 8, sum & 0x4 == 4, sum & 0x2 == 2, sum & 0x1 == 1, sum > 15 };
+        const sum = a + b + carry;
+        outputs[i] = .{ sum & 0x8 == 8, sum & 0x4 == 4, sum & 0x2 == 2, sum & 0x1 == 1, sum > 15 };
 
-//         var simulator = try Simulator.init(text_netlist, std.testing.allocator);
-//         defer simulator.deinit();
+        var simulator = try Simulator.init(text_netlist, std.testing.allocator);
+        defer simulator.deinit();
 
-//         simulator.ports.items[0] = input_scenarios[i][0];
-//         simulator.ports.items[1] = input_scenarios[i][1];
-//         simulator.ports.items[2] = input_scenarios[i][2];
-//         simulator.ports.items[3] = input_scenarios[i][3];
-//         simulator.ports.items[4] = input_scenarios[i][4];
-//         simulator.ports.items[5] = input_scenarios[i][5];
-//         simulator.ports.items[6] = input_scenarios[i][6];
-//         simulator.ports.items[7] = input_scenarios[i][7];
-//         simulator.ports.items[8] = input_scenarios[i][8];
+        simulator.setNodeStateString("in_a3", input_scenarios[i][0]);
+        simulator.setNodeStateString("in_a2", input_scenarios[i][1]);
+        simulator.setNodeStateString("in_a1", input_scenarios[i][2]);
+        simulator.setNodeStateString("in_a0", input_scenarios[i][3]);
+        simulator.setNodeStateString("in_b3", input_scenarios[i][4]);
+        simulator.setNodeStateString("in_b2", input_scenarios[i][5]);
+        simulator.setNodeStateString("in_b1", input_scenarios[i][6]);
+        simulator.setNodeStateString("in_b0", input_scenarios[i][7]);
+        simulator.setNodeStateString("in_carry", input_scenarios[i][8]);
 
-//         try simulator.tick();
-//         try simulator.tick();
-//         //try simulator.tick();
-//         //try simulator.tick();
+        try simulator.tick();
+        try simulator.tick();
+        try simulator.tick();
+        try simulator.tick();
         
-//         // std.debug.print("a: {d} b: {d} carry: {d} sum: {d}\n", .{ a, b, carry, sum });
-//         // std.debug.print("{d} {d} {d} {d} {d} {d} {d} {d} {d} - {d} {d} {d} {d} {d}\n", .{
-//         //     @intFromBool(input_scenarios[i][0]),
-//         //     @intFromBool(input_scenarios[i][1]),
-//         //     @intFromBool(input_scenarios[i][2]),
-//         //     @intFromBool(input_scenarios[i][3]),
-//         //     @intFromBool(input_scenarios[i][4]),
-//         //     @intFromBool(input_scenarios[i][5]),
-//         //     @intFromBool(input_scenarios[i][6]),
-//         //     @intFromBool(input_scenarios[i][7]),
-//         //     @intFromBool(input_scenarios[i][8]),
-//         //     @intFromBool(outputs[i][0]),
-//         //     @intFromBool(outputs[i][1]),
-//         //     @intFromBool(outputs[i][2]),
-//         //     @intFromBool(outputs[i][3]),
-//         //     @intFromBool(outputs[i][4])
-//         //     });
-//         // std.debug.print("                  - {d} {d} {d} {d} {d}\n", .{
-//         //     @intFromBool(simulator.nodes.get("xor_7").?.state),
-//         //     @intFromBool(simulator.nodes.get("xor_6").?.state),
-//         //     @intFromBool(simulator.nodes.get("xor_5").?.state),
-//         //     @intFromBool(simulator.nodes.get("xor_4").?.state),
-//         //     @intFromBool(simulator.nodes.get("or_3").?.state)
-//         // });
-//         std.debug.print("{d} - {d}\n", .{ @intFromBool(simulator.nodes.get("or_1").?.state), @intFromBool(simulator.nodes.get("and_6").?.state)});
-//         try expect(simulator.nodes.get("xor_7").?.state == outputs[i][0]);
-//         try expect(simulator.nodes.get("xor_6").?.state == outputs[i][1]);
-//         try expect(simulator.nodes.get("xor_5").?.state == outputs[i][2]);
-//         try expect(simulator.nodes.get("xor_4").?.state == outputs[i][3]);
-//         try expect(simulator.nodes.get("or_3").?.state == outputs[i][4]);
-//     }
+        std.debug.print("a: {d} b: {d} carry: {d} sum: {d}\n", .{ a, b, carry, sum });
+        std.debug.print("{d} {d} {d} {d} {d} {d} {d} {d} {d} - {d} {d} {d} {d} {d}\n", .{
+            @intFromBool(input_scenarios[i][0]),
+            @intFromBool(input_scenarios[i][1]),
+            @intFromBool(input_scenarios[i][2]),
+            @intFromBool(input_scenarios[i][3]),
+            @intFromBool(input_scenarios[i][4]),
+            @intFromBool(input_scenarios[i][5]),
+            @intFromBool(input_scenarios[i][6]),
+            @intFromBool(input_scenarios[i][7]),
+            @intFromBool(input_scenarios[i][8]),
+            @intFromBool(outputs[i][0]),
+            @intFromBool(outputs[i][1]),
+            @intFromBool(outputs[i][2]),
+            @intFromBool(outputs[i][3]),
+            @intFromBool(outputs[i][4])
+            });
+        std.debug.print("                  - {d} {d} {d} {d} {d}\n", .{
+            @intFromBool(simulator.getNodeStateString("xor_7")),
+            @intFromBool(simulator.getNodeStateString("xor_6")),
+            @intFromBool(simulator.getNodeStateString("xor_5")),
+            @intFromBool(simulator.getNodeStateString("xor_4")),
+            @intFromBool(simulator.getNodeStateString("or_3"))
+        });
+        std.debug.print("{d} - {d}\n", .{
+            @intFromBool(simulator.getNodeStateString("or_1")),
+            @intFromBool(simulator.getNodeStateString("and_6"))
+        });
+    
+        try expect(simulator.getNodeStateString("xor_7") == outputs[i][0]);
+        try expect(simulator.getNodeStateString("xor_6") == outputs[i][1]);
+        try expect(simulator.getNodeStateString("xor_5") == outputs[i][2]);
+        try expect(simulator.getNodeStateString("xor_4") == outputs[i][3]);
+        try expect(simulator.getNodeStateString("or_3") == outputs[i][4]);
+    }
 
-// }
+}
 
 // test "adder" {
 //     const text_netlist: [*:0]const u8 =
